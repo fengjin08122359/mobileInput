ce448430260cd4eb91ed682ff2cd17bbe6093863
mike <fj110808@163.com>
2018-06-11 11:11:53 +0800

    describe change

--- mobileInput.js
+++ mobileInput.js
@@ -1,3 +1,17 @@
+/*!
+* 1.该插件针对于手机端输入框点击后输入法唤起的适配
+* 2.该插件依赖于jQuery
+* 3.使用时必须设置inputId(输入框的ID)
+* 使用样例:
+* var mobileInput = $.mobileInput({inputId: "message"});
+* $("#message").on('focus', function() {
+* mobileInput.startCheck();
+* }).on('blur', function() {
+* mobileInput.end();
+* });
+* Licensed under the MIT license
+*/
+
 (function(window, $, undefined) {
   var OS= (function(navigator, userAgent, platform, appVersion){
     var detect = {}
