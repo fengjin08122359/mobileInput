db2faa09203f5d144a9f90b2c7a722f3de63c815
fengjin08122359 <fj110808@163.com>
2018-11-16 16:22:06 +0800

    again

--- mobileInput.js
+++ mobileInput.js
@@ -200,6 +200,13 @@
       var m = this;
       this.checkStatus = 2;
       console.log("startCheckTime" + new Date().Format("hh:mm:ss"));
+      if (m.special){
+        $("html").css({
+          top: 0,
+          width: "100%",
+          height: height-m.special
+        });
+      }
       this.changeCheck();
     },
     isStatusChange:function(){
